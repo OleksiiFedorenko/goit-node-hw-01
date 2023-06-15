3c5bea0b3fe43cee34b757c6a9cab7cbbf6e8a0d
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-06-15 23:49:19 +0300

    Finalize functions

--- contacts.js
+++ contacts.js
@@ -1,37 +1,42 @@
 const fs = require('fs/promises');
-const path = require('node:path');
+const path = require('path');
+const { nanoid } = require('nanoid');
 
-const contactsPath = path.join(__dirname, 'db/contacts.json');
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
-async function listContacts() {
+const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-  console.log(JSON.parse(data));
-}
+const getContactById = async id => {
+  const contacts = await listContacts();
+  return contacts.find(contact => contact.id === id) || null;
+};
 
-async function getContactById(contactId) {
-  const data = await fs.readFile(contactsPath);
-  console.log(data);
-}
+const addContact = async data => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
-async function removeContact(contactId) {
-  const data = await fs.readFile(contactsPath);
-  console.log(data);
-}
-
-async function addContact(name, email, phone) {
-  const data = await fs.readFile(contactsPath);
-  console.log(data);
-}
-
-function hi() {
-  console.log('hi');
-}
+const removeContact = async id => {
+  const contacts = await listContacts();
+  const indexToRemove = contacts.findIndex(contact => contact.id === id);
+  if (indexToRemove === -1) return null;
+  const [removedContact] = contacts.splice(indexToRemove, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return removedContact;
+};
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  hi,
 };
--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  hi,
-} = require('./contacts.js');
+const contacts = require('./contacts.js');
 
-listContacts();
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'getAll':
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+
+    case 'getById':
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+
+    case 'removeById':
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    case 'add':
+      const addedContact = await contacts.addContact({ name, email, phone });
+      console.log(addedContact);
+      break;
+
+    default:
+      console.log('Unknown action');
+      break;
+  }
+};
+
+// invokeAction({ action: 'getAll' });
+// invokeAction({ action: 'getById', id: 'rsKkOQUi80UsgVPCcLZZW' });
+// invokeAction({ action: 'removeById', id: 'rsKkOQUi80UsgVPCcLZZW' });
+invokeAction({
+  action: 'add',
+  name: 'Alec Howard',
+  email: 'Donec.elementum@scelerisquescelerisquedui.net',
+  phone: '(748) 206-2688',
+});
